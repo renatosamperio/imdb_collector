9951e3242ecc8119bf1820474fe9ff6ef8451e7f
Renato Samperio <renatosamperio@gmail.com>
2018-06-18 13:10:13 +0200

    IMDB_COLLECTOR: Added torrent crawling method

--- src/imdb_collector.py
+++ src/imdb_collector.py
@@ -141,6 +141,111 @@ class CollectIMDb(ros_node.RosNode):
         finally:
             return self.invoice
 
+    def CrawlDbRecords(self):
+        records = None
+        try:
+            
+            ## Establishing DB connection
+            if self.data_collection is None:
+                rospy.logwarn("Invalid collection name: [%s]"%self.data_collection)
+                return
+            if self.data_database is None:
+                rospy.logwarn("Invalid database name: [%s]"%self.data_database)
+                return
+                
+            rospy.logdebug("1.1 Establishing DB connection to [%s/%s]"%
+                              (self.data_database, self.data_collection))
+            db_handler      = MongoAccess()
+            is_connected    = db_handler.Connect(self.data_database, self.data_collection)
+            if not is_connected:
+                rospy.logwarn("Invalid connection to [%s/%s] "%
+                              (self.data_database, self.data_collection))
+                return
+            
+            ## Querying torrents without IMDb data
+            rospy.logdebug("1.2 Querying torrents without IMDb data")
+            query           = None ## TODO: Search for IMDB identifier
+            records         = db_handler.Find(query, timeout=True)
+            records_found   = records.count()
+            rospy.logdebug("      Found [%d] records"%records_found)
+            if records_found<1:
+                rospy.logdebug("   + No records require IMDb update")
+                
+            ## Looking into found records
+            rospy.logdebug("1.3 Updating records")
+            counter         = 1
+            record_counter  = 0
+            for record in records:
+                record_counter += 1
+                #title       = record['name']
+                title       = u''.join(record['name']).encode('utf-8').strip()
+                if 'Autobahnpolizei' in title:
+                    print "===> THIS FAILED!!!"
+                rospy.logdebug("  1.3.1 [%s] Getting cleaned movie title [%s]"%
+                               (str(record_counter), title))
+                clean_name  = self.imdb_handler.clean_sentence(title)
+                if len(clean_name) < 1:
+                    rospy.logdebug("   + Invalid cleaned title")
+                    continue
+                
+                year_found, splitted = self.imdb_handler.skim_title(clean_name)
+                if splitted is None:
+                    rospy.logdebug("   + Invalid skimmed title")
+                    continue
+                elif len(splitted)<1:
+                    rospy.logwarn('No title was given for [%s]'%title)
+                    continue
+                rospy.logdebug("   + Skimmed title: [%s]"%(splitted))
+                
+                ## Searching if item already exists
+                rospy.logdebug("  1.3.2 Searching if item [%s] already exists"%splitted)
+                title_exists = self.db_handler.Find({ "query_title": splitted})
+                if title_exists.count():
+                    rospy.logdebug("        Title [%s] already exists"%splitted)
+                    continue
+                
+                ## Collecting IMDb data
+                rospy.logdebug("  1.3.3 Collecting best title of IMDb data")
+                items       = self.imdb_handler.get_imdb_best_title(splitted, year_found=year_found)
+                item_keys   = items.keys()
+                if len(items)<1:
+                    rospy.logwarn("No IMDb info was found for [%s]"%splitted)
+                    continue
+                    
+                ## Showing multiple items
+                if "imdb_info" in item_keys and len(items["imdb_info"])<1:
+                    rospy.logwarn("No IMDb info was found for [%s]"%splitted)
+                    continue
+                
+                if "imdb_info" in item_keys and len(items["imdb_info"])>1:
+                    rospy.loginfo("Items [%s] found"%(str(len(items["imdb_info"]))))
+                    for item in items["imdb_info"]:
+                        rospy.loginfo("   title [%s] with score [%s]"%
+                                      (str(item['title']), str(item['score'])))
+
+                ## Updating IMDb data
+                query_title = None
+                if 'query_title'in item_keys:
+                    query_title = items['query_title']
+                score_ = float(items['imdb_info'][0]['score'])
+                rospy.loginfo("  1.3.4 Inserted [%d] [%s] into DB with score [%2.4f]"%
+                              (counter, str(query_title), score_))
+                post_id = self.db_handler.Insert(items)
+                
+                #pprint(items)
+                counter += 1
+                if counter > self.retrieved_limit:
+                    break
+            
+            rospy.loginfo("Closing, mongo cursor as it has timed out")
+            records.close()
+        except Exception as inst:
+              utilities.ParseException(inst)
+        finally:
+            if records is not None:
+                rospy.loginfo("Re-closing mongo cursor...")
+                records.close()
+
 if __name__ == '__main__':
     usage       = "usage: %prog option1=string option2=bool"
     parser      = OptionParser(usage=usage)
